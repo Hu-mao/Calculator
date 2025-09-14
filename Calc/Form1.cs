0139b0abf3ee6f94bdf6566c67bb26798884671f
IllyaSP <plantsvs233zmbie@gmail.com>
2025-09-14 16:43:35 +0300

    WorkPlus

--- Calc/Form1.cs
+++ Calc/Form1.cs
@@ -15,20 +15,19 @@ namespace Calc
     public partial class Form1 : Form
     {
         string[] actions = new string[2];
-        decimal? num1, num2;
+        decimal? num1 = 0, num2;
         char action;
-        bool toclear = false, cleared = false, torewrite = false, canrewrite = false;
+        bool toclear = false, cleared = true, torewrite = false, canrewrite = false, canwrite = false;
         public Form1()
         {
             InitializeComponent();
         }
         void dii(decimal a) 
         {
-            if (toclear) { label1.Text = ""; toclear = false; cleared = true; }
-            if(canrewrite){label2.Text = " ";canrewrite = false;}
-            num1 = a;
+            if (toclear) { label1.Text = ""; toclear = false; cleared = true; canwrite = true; }
+            //if(canrewrite){label2.Text = " ";canrewrite = false;}
             label1.Text += a.ToString();
-            torewrite = true;
+            //torewrite = true;
         }
         private void button9_Click(object sender, EventArgs e)
         {
@@ -80,43 +79,42 @@ namespace Calc
 
         private void button25_Click(object sender, EventArgs e)
         {
+            if (toclear) { label1.Text = ""; toclear = false; cleared = true; canwrite = true; }
             if (label1.Text == "") return;
             if (label1.Text.Split(',').Count() > 2) return;
             else label1.Text += ",";
         }
-
+        void fornum1() {
+            if (num1 == null)
+            {
+                if (actions.Count() == 2) num1 = (decimal.Parse($"{actions[0]},{actions[1]}"));
+                else if (actions.Count() == 1) num2 = decimal.Parse($"{actions[0]}");
+                label2.Text = num1.ToString() + " " + action;
+            }
+        }
+        decimal? LabToDec()
+        {
+            if (actions.Count() == 2)  return(decimal.Parse($"{actions[0]},{actions[1]}"));
+            if (actions.Count() == 1) return decimal.Parse($"{actions[0]}");
+            else return null;
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             action = '+';
             if (label1.Text != "") actions = label1.Text.Split(',');
-            if (num1 == null)
+            if (label2.Text == "")
             {
-                if (actions.Count() == 2) num1 = (decimal.Parse($"{actions[0]},{actions[1]}"));
-                else if (actions.Count() == 1) num1 = decimal.Parse($"{actions[0]}");
+                num1 = LabToDec();
                 label2.Text = num1.ToString() + " " + action;
+                toclear = true;
             }
-            else if (num1 + num2 == decimal.Parse(label1.Text) || torewrite)
+            else if (cleared && canwrite && num1 != null && num2 == null)
             {
-                num1 = decimal.Parse(label1.Text);
-                num2 = null;
-                label2.Text = label1.Text + " " + action;
-                torewrite = false;
-            }
-            else if (num2 != null)
-            {
-                num2 = decimal.Parse(label1.Text);
-                num1 += num2;
-                label2.Text = num1.ToString() + " " + action;
-                label1.Text = num1.ToString();
-            }
-            else
-            {
-                if (cleared) num1 += decimal.Parse(label1.Text);
-                label2.Text = num1.ToString() + " " + action;
 
-                cleared = false;
+                num2 = LabToDec();
+                label2.Text = (num1 + LabToDec()).ToString() + " " + action;
+                toclear = true;
             }
-            toclear = true;
         }
 
         private void button8_Click(object sender, EventArgs e)
@@ -129,68 +127,53 @@ namespace Calc
                 else if (actions.Count() == 1) num1 = decimal.Parse($"{actions[0]}");
                 label2.Text = num1.ToString() + " " + action;
             }
-            else if (num1 + num2 == decimal.Parse(label1.Text) || torewrite)
+            else if (num1 + num2 == LabToDec())
             {
-                num1 = decimal.Parse(label1.Text);
+                num1 = LabToDec();
                 num2 = null;
                 label2.Text = label1.Text + " " + action;
                 torewrite = false;
             }
             else if (num2 != null)
             {
-                num2 = decimal.Parse(label1.Text);
+                num2 = LabToDec();
                 num1 -= num2;
                 label2.Text = num1.ToString() + " " + action;
                 label1.Text = num1.ToString();
             }
             else
             {
-                if (cleared) num1 -= decimal.Parse(label1.Text);
+                if (cleared) num1 -= LabToDec();
                 label2.Text = num1.ToString() + " " + action;
 
                 cleared = false;
             }
             toclear = true;
         }
-        decimal? ActionToDo(decimal? a, decimal? b) 
-        {
-            switch (action) 
-            {
-                case '+': return a + b;
-                case '-': return a + b;
-                case '*': return a + b;
-                case '/': return Func.Delit(a, b);
-                case '^': return a*a;
-                case '%': return a /100;
-                case '1': return Func.Drob(a);
-                case '√': return Func.Kor(a);
-                default: return null;
-            }
-        }
         private void button16_Click(object sender, EventArgs e)
         {
-            if (ActionToDo(num1, num2) == null) return;
-            if (torewrite)
-            {
-                num1 = decimal.Parse(label1.Text) + num2;
-                label2.Text = label1.Text + " " + action + " " + num2.ToString() + " =";
-                label1.Text = num1.ToString();
-                torewrite = false;
-            }
-            else if ((cleared && num2 != null) || num2 == null)
-            {
-                num2 = decimal.Parse(label1.Text);
-                label2.Text = num1.ToString() + " " + action + " " + num2.ToString() + " =";
-                label1.Text = (num1 + num2).ToString();
-                toclear = true; cleared = false;
-            }
-            else
-            {
-                num1 += num2;
-                label2.Text = num1.ToString() + " " + action + " " + num2.ToString() + " =";
-                label1.Text = (num1 + num2).ToString();
-                toclear = true; cleared = false;
-            } canrewrite = true;
+            
+            //if ((cleared && num2 != null) || num2 == null)
+            //{
+            //    num2 = decimal.Parse(label1.Text);
+            //    label2.Text = num1.ToString() + " " + action + " " + num2.ToString() + " =";
+            //    label1.Text = (num1 + num2).ToString();
+            //    toclear = true; cleared = false;
+            //}
+            //else if (torewrite)
+            //{
+            //    num1 = decimal.Parse(label1.Text) + num2;
+            //    label2.Text = label1.Text + " " + action + " " + num2.ToString() + " =";
+            //    label1.Text = num1.ToString();
+            //    torewrite = false;
+            //}
+            //else
+            //{
+            //    num1 += num2;
+            //    label2.Text = num1.ToString() + " " + action + " " + num2.ToString() + " =";
+            //    label1.Text = (num1 + num2).ToString();
+            //    toclear = true; cleared = false;
+            //} canrewrite = true;
         }
     }
 }
